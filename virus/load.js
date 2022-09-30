bd26e9f79ec2505fabab0811de5fdad3f016a3ba
a1roman <102273010+a1roman@users.noreply.github.com>
2022-09-30 18:13:54 +0800

    Add files via upload

---
+++ virus/load.js
@@ -0,0 +1,25 @@
+class Load {
+	preload() {
+	  this.load.image('background', 'assets/background.png');
+      this.load.image('player', 'assets/player.png');
+	  this.load.image('coin', 'assets/coin.png');
+	  this.load.image('enemy', 'assets/enemy.png');
+      this.load.image('ball', 'assets/ball.png');	 
+	  this.load.image('tileset', 'assets/tileset.png');
+      this.load.tilemapTiledJSON('map', 'assets/map.json');	 
+
+  
+	// Display a loading label
+	let loadLabel = this.add.text(150, 230, 'loading',
+	{ font: '30px Arial', fill: '#fff' });
+	
+	// Change origin point of the Text
+	// Centred the Text on the screen
+	loadLabel.setOrigin(0.5, 0.5);
+  }
+  
+  create() {
+	// Start the menu scene
+	this.scene.start('menu');
+  }
+}
