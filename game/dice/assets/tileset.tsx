634668769bfc5851460a36f87441de4f45add36a
a1roman <102273010+a1roman@users.noreply.github.com>
2022-09-30 18:41:28 +0800

    Add files via upload

---
+++ game/dice/assets/tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="tileset" tilewidth="20" tileheight="5" tilecount="1" columns="1">
+ <image source="tileset.png" width="20" height="5"/>
+</tileset>
